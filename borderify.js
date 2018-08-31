0920451c32f6605b9f030de569933c8712d14a8d
spiderwebpoc <42878012+spiderwebpoc@users.noreply.github.com>
2018-08-31 21:53:07 +0100

    Only blocks www.google.co.uk/ as url shown by google

--- borderify.js
+++ borderify.js
@@ -1,9 +1,11 @@
-const list1 = document.getElementsByClassName("r"); //Get All Google Search Results
+const list1 = document.getElementsByClassName("rc"); //Get All Google Search Results
 
 
 for(i=0; i<list1.length; i++){ //iterate.
-list1[i].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
-list1[i].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>" //Add the warning
+if(list1[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML == "www.google.co.uk/"){
+list1[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
+list1[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>" //Add the warning
+}
 }
 
 const list2 = document.getElementsByClassName("rg_ic rg_i"); //Get all google images
