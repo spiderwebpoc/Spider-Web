e87a1be2ca6fef4d0ff69ffe18a5b88e02eb332e
spiderwebpoc <42878012+spiderwebpoc@users.noreply.github.com>
2018-09-01 00:36:16 +0100

    Obvious bug fixing

--- SpiderWeb.js
+++ SpiderWeb.js
@@ -1,5 +1,5 @@
 const search = document.getElementsByClassName("rc"); //Get All Google Search Results
-var badsites[]; //List of bad sites seperated by comma
+var badsites = []; //List of bad sites seperated by comma
 
 for(i=0; i<search.length; i++){ //iterate.
 try { //Deal with googles inconsistent URL formatting
@@ -16,10 +16,10 @@ search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML 
 }
 }
 }
-
-const image = document.getElementsByClassName("rg_bx rg_di rg_el ivg-i"); //Get all google images
+const image = document.getElementsByClassName("rg_l"); //Get all google images
 for(i=0; i<image.length; i++){ //iterate.
+//alert(image[i].getElementsByClassName("rg_ilmbg")[0].innerHTML.split("-")[1].trim());
 if(badsites.includes(image[i].getElementsByClassName("rg_ilmbg")[0].innerHTML.split("-")[1].trim())){ //Identify images from urls on the ban list
-image[i].getElementsByClassName("rg_l")[0].getElementsByClassName("rg_ic rg_i")[0].src="https://media.discordapp.net/attachments/483662916449468418/484359949703577632/6006.png?width=994&height=560" //Swap image with generic blur
+image[i].getElementsByClassName("rg_ic rg_i")[0].src="https://media.discordapp.net/attachments/483662916449468418/484359949703577632/6006.png?width=994&height=560" //Swap image with generic blur
 }
 }
