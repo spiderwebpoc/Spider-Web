9ccbb959b6f5da888539a31db2350f07cc16ac48
spiderwebpoc <42878012+spiderwebpoc@users.noreply.github.com>
2018-09-01 00:16:56 +0100

    Pollished a turd.

--- SpiderWeb.js
+++ SpiderWeb.js
@@ -1,11 +1,19 @@
 const search = document.getElementsByClassName("rc"); //Get All Google Search Results
-var badsites=[""]; //List of bad sites seperated by comma
+var badsites[]; //List of bad sites seperated by comma
 
 for(i=0; i<search.length; i++){ //iterate.
-
+try { //Deal with googles inconsistent URL formatting
+//alert(new URL(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML).hostname);
 if(badsites.includes(new URL(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML).hostname)){ //find search results on the ban list
 search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
-search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>" //Add the warning
+search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>"; //Add the warning
+}
+}
+catch(err){ //Deal with googles inconsistent URL formatting
+if(badsites.includes(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML)){ //find search results on the ban list
+search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
+search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>"; //Add the warning
+}
 }
 }
 
