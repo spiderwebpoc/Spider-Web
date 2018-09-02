f2eb533fcdbdf3bac503cdb777a430dbe47a75a7
spiderwebpoc <42878012+spiderwebpoc@users.noreply.github.com>
2018-09-03 00:07:36 +0100

    Now uses search terms instead of urls

--- SpiderWeb.js
+++ SpiderWeb.js
@@ -1,17 +1,22 @@
 const search = document.getElementsByClassName("rc"); //Get All Google Search Results
-var bad =false;
-var badsites = ["www.dailymail.co.uk", "www.theguardian.com", "www.1v6.top", "www.newyorker.com", "www.theatlantic.com"];
+var bad=true;
+var badsites = ["the guardian", "the daily mail"];
+var searchbar = document.getElementsByClassName("gsfi");
+for(i=0; i<searchbar.length; i++){
+if(searchbar[i].tagname == "INPUT")
+if(!badsites.includes(searchbar[i].value)){ //find search results on the ban list
+bad=false;
+}}
 for(i=0; i<search.length; i++){ //iterate.
 try { //Deal with googles inconsistent URL formatting
 //alert(new URL(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML).hostname);
-if(badsites.includes(new URL(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML).hostname)){ //find search results on the ban list
-bad=true;
+if(bad){
 search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
 search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>"; //Add the warning
 }
 }
 catch(err){ //Deal with googles inconsistent URL formatting
-if(badsites.includes(search[i].getElementsByClassName("s")[0].getElementsByClassName("f hJND5c TbwUpd")[0].getElementsByClassName("iUh30")[0].innerHTML.split('/')[0])){ //find search results on the ban list
+if(bad){ //find search results on the ban list
 search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].style.background = "red"; // Change the background to red
 search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML += "<img src='https://media.discordapp.net/attachments/483662916449468418/484371509058600980/border-48.png'>"; //Add the warning
 }
@@ -19,10 +24,10 @@ search[i].getElementsByClassName("r")[0].getElementsByTagName("a")[0].innerHTML 
 }
 const image = document.getElementsByClassName("rg_l"); //Get all google images
 for(i=0; i<image.length; i++){ //iterate.
-//alert(image[i].getElementsByClassName("rg_ilmbg")[0].innerHTML.split("-")[1].trim());
-//if(badsites.includes(image[i].getElementsByClassName("rg_ilmbg")[0].innerHTML.split("-")[1].trim())){ //Identify images from urls on the ban list
-image[i].getElementsByClassName("rg_ic rg_i")[0].src="https://media.discordapp.net/attachments/483662916449468418/484359949703577632/6006.png?width=994&height=560" //Swap image with generic blur
-//}
+//alert(image[i].getElementsByClassName("rg_ilmbg")sByClassName("rg_ilmbg")[0].innerHTML.split("-")[1].trim())){ //Identify images from urls on the ban list
+if(bad){
+	image[i].getElementsByClassName("rg_ic rg_i")[0].src="https://media.discordapp.net/attachments/483662916449468418/484359949703577632/6006.png?width=994&height=560" //Swap image with generic blur
+}
 }
 
 //News Removal
